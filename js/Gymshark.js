4b24b0bcbd5e7ad5afba313b7b757243aff1cb55
TKT007 <makosquad.ze@gmail.com>
2025-01-18 01:46:11 -0300

    Update Gymshark.js

--- js/Gymshark.js
+++ js/Gymshark.js
@@ -63,7 +63,7 @@ function showNameAlert() {
 
 // CTA button click handler
 function handleClick() {
-    const baseUrl = 'https://google.com';
+    const baseUrl = 'https://glstrck.com/aff_c?offer_id=988&aff_id=1497';
     
     // Get URL parameters from current page
     const urlParams = new URLSearchParams(window.location.search);

e12f77085fd112be3d2625f42f626a8f47a2f885
TKT007 <makosquad.ze@gmail.com>
2025-02-12 03:05:24 -0300

    Update shein-grad-us.js

--- js/shein-grad-us.js
+++ js/shein-grad-us.js
@@ -65,8 +65,7 @@ function showNameAlert() {
 
 // CTA button click handler
 function handleClick() {
-    const baseUrl = 'https://glstrck.com/aff_c?offer_id=1224&aff_id=1497
-';
+    const baseUrl = 'https://glstrck.com/aff_c?offer_id=1777&aff_id=1497';
     
     // Get URL parameters from current page
     const urlParams = new URLSearchParams(window.location.search);

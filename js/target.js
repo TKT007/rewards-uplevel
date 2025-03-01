a0972cdde3e5aa6d66c93ba7046939ef76c4509d
TKT007 <makosquad.ze@gmail.com>
2025-03-01 04:32:40 -0300

    Update target.js

--- js/target.js
+++ js/target.js
@@ -63,7 +63,7 @@ function showNameAlert() {
 
 // CTA button click handler
 function handleClick() {
-    const baseUrl = 'https://glstrck.com/aff_c?offer_id=740&aff_id=88388';
+    const baseUrl = 'https://glstrck.com/aff_c?offer_id=740&aff_id=1497';
     
     // Get URL parameters from current page
     const urlParams = new URLSearchParams(window.location.search);

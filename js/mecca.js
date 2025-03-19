79576b8f5cd573f828735781199247b54fae62af
TKT007 <makosquad.ze@gmail.com>
2025-03-18 22:35:13 -0300

    Update mecca.js

--- js/mecca.js
+++ js/mecca.js
@@ -63,7 +63,7 @@ function showNameAlert() {
 
 // CTA button click handler
 function handleClick() {
-    const baseUrl = 'https://glstrck.com/aff_c?offer_id=144&aff_id=88388';
+    const baseUrl = 'https://glitchy.go2cloud.org/aff_c?offer_id=2013&aff_id=1497';
     
     // Get URL parameters from current page
     const urlParams = new URLSearchParams(window.location.search);

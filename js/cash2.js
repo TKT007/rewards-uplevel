148f38a2200571f120d4d56bf8b599fc3c5b8527
TKT007 <makosquad.ze@gmail.com>
2025-03-28 21:25:26 -0300

    Update cash2.js

--- js/cash2.js
+++ js/cash2.js
@@ -63,7 +63,7 @@ function showNameAlert() {
 
 // CTA button click handler
 function handleClick() {
-    const baseUrl = 'https://glstrck.com/aff_c?offer_id=671&aff_id=1497';
+    const baseUrl = 'https://glstrck.com/aff_c?offer_id=1084&aff_id=1497';
     
     // Get URL parameters from current page
     const urlParams = new URLSearchParams(window.location.search);
